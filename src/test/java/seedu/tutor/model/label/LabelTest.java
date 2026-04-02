4ee45b25ca07352ac25c3d75eb71e0f3a34f2638
brendaan07 <brendaaaan07@gmail.com>
2026-04-02 13:35:27 +0800

    Remove unnecessary Imports

--- src/test/java/seedu/tutor/model/label/LabelTest.java
+++ src/test/java/seedu/tutor/model/label/LabelTest.java
@@ -1,6 +1,5 @@
 package seedu.tutor.model.label;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.tutor.testutil.Assert.assertThrows;
